14d74c652ee0670ee2cdb6fd6ce43854800f4f9f
cmakihara <clmakihara@gmail.com>
2022-03-24 09:30:58 -0300

    Commit Fibonacci

--- src/functions/Fibonacci/index.ts
+++ src/functions/Fibonacci/index.ts
@@ -6,6 +6,18 @@
  * @example Input: N = 5
  *          Output: [0, 1, 1, 2, 3]
  */
-export const fibonacci = (N: number): Array<number> => {
-  // TODO: Implementar a função e testar a chamada dela no index.ts
+ export const fibonacci = (N: number): Array<number> => {
+  let number = new Array();
+  let proximo = 1;
+  let atual = 0;
+  
+  for(let i = 0; i < N-1; i++) {    
+    if(i === 0){
+      number.push(0);    
+    }
+    number.push(proximo);
+    atual = proximo;
+    proximo = proximo + number[i];
+  }
+  return number;
 }
