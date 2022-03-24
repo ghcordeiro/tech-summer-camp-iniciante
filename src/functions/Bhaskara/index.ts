f70de2da98ba9860e806277f81db1cc2949d7e95
cmakihara <clmakihara@gmail.com>
2022-03-24 09:30:31 -0300

    Commit Bhaskara

--- src/functions/Bhaskara/index.ts
+++ src/functions/Bhaskara/index.ts
@@ -17,7 +17,11 @@ interface IBhaskaraResponse {
  * @example Input: a: 10, b: 20.1 c: 5.1, 
  *          Output: x1: -0.2978, x2: -1.7121
  */
-export const bhaskara = (a: number, b: number, c: number): IBhaskaraResponse => {
-  // TODO: Implementar a função e testar a chamada dela no index.ts
- 
+ export const bhaskara = (a: number, b: number, c: number): IBhaskaraResponse => {
+  let coeficiente1;
+  let coeficiente2;
+  let delta = (b * b) - 4 * a * c;
+  coeficiente1 = ((-b + Math.sqrt(delta)) / (2 * a)).toFixed(4);
+  coeficiente2 = ((-b - Math.sqrt(delta)) / (2 * a)).toFixed(4); 
+  return  { x1: Number(coeficiente1), x2: Number(coeficiente2) }  ;
 }
